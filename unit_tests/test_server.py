7532155aee43e18cc0a919a3519be6b5df4bd83b
GudwinAO <92263753+GudwinAO@users.noreply.github.com>
2023-01-02 11:41:38 +0400

    Lesson_5_try (#5)
    Co-authored-by: Aleksandr <gudwinao88@gmail.ru>

--- unit_tests/test_server.py
+++ unit_tests/test_server.py
@@ -1,35 +1,21 @@
-import unittest
-import os
+"""Unit-тесты сервера"""
+
 import sys
-import time
-sys.path.append(os.path.join(os.getcwd(),".."))
+import os
+import unittest
+sys.path.append(os.path.join(os.getcwd(), '..'))
+from common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE
 from server import process_client_message
-from common import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE
 
 class TestServer(unittest.TestCase):
-
+    '''
+    В сервере только 1 функция для тестирования
+    '''
     err_dict = {
         RESPONSE: 400,
         ERROR: 'Bad Request'
     }
-
     ok_dict = {RESPONSE: 200}
-
-    def setUp(self):
-
-        self.presence_success_msg = {
-                    ACTION: PRESENCE,
-                    TIME: time.time(),                    
-                    USER: {
-                            ACCOUNT_NAME: 'Guest',
-                         }
-                    }
-
-
-    def test_process_client_message_dict(self):
-        self.assertIsInstance(process_client_message(self.presence_success_msg), dict)
-
-    #from practic, no changes    
 
     def test_no_action(self):
         """Ошибка если нет действия"""
@@ -54,17 +40,12 @@ class TestServer(unittest.TestCase):
     def test_unknown_user(self):
         """Ошибка - не Guest"""
         self.assertEqual(process_client_message(
-            {ACTION: PRESENCE, TIME: '1.1', USER: {ACCOUNT_NAME: 'Guest1'}}), self.err_dict)
+            {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest1'}}), self.err_dict)
 
     def test_ok_check(self):
         """Корректный запрос"""
         self.assertEqual(process_client_message(
-            {ACTION: PRESENCE, TIME: '1.1', USER: {ACCOUNT_NAME: 'Guest'}}), self.ok_dict)
-
-    #variant 2
-
-    def test_process_client_message_presence(self):
-        self.assertEqual(process_client_message(self.presence_success_msg)['response'], 200)
+            {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.ok_dict)
 
 
 if __name__ == '__main__':
