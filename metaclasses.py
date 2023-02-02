5edc37b4544c0e4d24c1b9ef133645c0fe391ab2
Aleksandr <gudwinao88@gmail.ru>
2023-02-02 16:53:03 +0400

    lesson_12

--- metaclasses.py
+++ metaclasses.py
@@ -63,6 +63,38 @@ class ClientMaker(type):
     def __init__(self, clsname, bases, clsdict):
         # Список методов, которые используются в функциях класса:
         methods = []
+        # Атрибуты, вызываемые функциями классов
+        attrs = []
+        for func in clsdict:
+            # Пробуем
+            try:
+                ret = dis.get_instructions(clsdict[func])
+                # Если не функция то ловим исключение
+            except TypeError:
+                pass
+            else:
+                # Раз функция разбираем код, получая используемые методы.
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+                    elif i.opname == 'LOAD_ATTR':
+                        if i.argval not in attrs:
+                            attrs.append(i.argval)
+        # Если обнаружено использование недопустимого метода accept, listen, socket бросаем исключение:
+        for command in ('accept', 'listen', 'socket'):
+            if command in methods:
+                raise TypeError('В классе обнаружено использование запрещённого метода')
+        # Если сокет не инициализировался константами SOCK_STREAM(TCP) AF_INET(IPv4), тоже исключение.
+        if not ('SOCK_STREAM' in attrs and 'AF_INET' in attrs):
+            raise TypeError('Некорректная инициализация сокета.')
+        super().__init__(clsname, bases, clsdict)
+
+# Метакласс для проверки корректности клиентов:
+class ClientMaker(type):
+    def __init__(cls, clsname, bases, clsdict):
+        # Список методов, которые используются в функциях класса:
+        methods = []
         for func in clsdict:
             # Пробуем
             try:
@@ -80,6 +112,9 @@ class ClientMaker(type):
         for command in ('accept', 'listen', 'socket'):
             if command in methods:
                 raise TypeError('В классе обнаружено использование запрещённого метода')
+        
+
+
         # Вызов get_message или send_message из utils считаем корректным использованием сокетов
         if 'get_message' in methods or 'send_message' in methods:
             pass
